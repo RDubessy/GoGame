5674dc0a8a7bf746541c0664bf400dea6536f5e4
Romain Dubessy <romain.dubessy_git@m4x.org>
2011-08-07 16:12:54 +0200

    Class ListOfGroups now documented

--- src/listofgroups.h
+++ src/listofgroups.h
@@ -3,12 +3,27 @@
 #include "group.h"
 class Stone;
 template <class T> class List;
+/*! This class represents a list of groups.
+  * All the stones of a player are stored as a list of groups.
+  * Inherits from the list template but has some more convenience functions.
+  */
 class ListOfGroups : public List<Group> {
     public:
+        /*! Default (and trivial) constructor. */
         ListOfGroups() : List<Group>() { };
+        /*! Print method. */
         void print();
+        /*! This method add a stone to the list of groups.
+          * It keeps track of the stones degrees of freedom and jailed nodes.
+          */
         bool add(Stone &stone,List<Stone> &freedom,List<Stone> &jail);
+        /*! This method signals to all groups in the list that a node has been
+          * taken by the oponent and is jailed.
+          */
         void jail(Stone &stone);
+        /*! This method signals to all groups in the list that a list of nodes
+          * have been freed.
+          */
         void freed(List<Stone> *stones);
 };
 #endif // LISTOFGROUPS_H
